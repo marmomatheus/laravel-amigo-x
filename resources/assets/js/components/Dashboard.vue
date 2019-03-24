5a65077343990043d2b32ae3af29489779ccb558
Matheus Marmo <matheusmarmo55@gmail.com>
2019-03-24 20:40:36 -0300

    sorteios na home

--- resources/assets/js/components/Dashboard.vue
+++ resources/assets/js/components/Dashboard.vue
@@ -4,16 +4,23 @@
 			<v-flex xs12 class="headline my-4 ml-2">
 				<b>Olá, {{ username }}</b><br>
 				<span class="subheading">Aqui aparecerão os ultimos sorteios que você está participando.</span>
+				<games></games>
 			</v-flex>
 		</v-layout>
 	</v-container>
 </template>
 
-<script>
+<script>	
+	import Games from './Modules/Game/List';
+
 	export default {
     	data: () => ({
     		username: null
 		}),
+		components: {
+			'games': Games
+		},
+
 		methods: {
 			setName() {
 				let user = JSON.parse(localStorage.getItem('user'));
